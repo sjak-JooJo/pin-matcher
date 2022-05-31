884faa1c0ba4b1bd80e164ba7b215fb341bf8142
JooJo <sjak0101@gmail.com>
2022-05-31 18:43:29 +0100

    updated calculation part

--- js/app.js
+++ js/app.js
@@ -14,3 +14,20 @@ function generatePin(){
    const pin = getPin();
    document.getElementById('display-pin').value = pin;
 }
+
+document.getElementById('key-pad').addEventListener('click', function(event){
+  const number = event.target.innerText;
+  const calcInput = document.getElementById('typed-numbers');
+  if(isNaN(number)){
+     if(number == 'C'){
+         calcInput.value = ' ';
+     }
+  }
+  else{
+    
+    const previousNumber = calcInput.value;
+    const newNumber = previousNumber + number;
+    calcInput.value = newNumber;
+  }
+  
+});
